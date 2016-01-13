7a94970de2024da9691e68ee884b71d84b4c15e6
Louis DEKEISTER <louis@hopstudios.com>
2016-01-13 11:13:29 -0800

    EE3 base setup

---
+++ ee3x/system/user/addons/hop_social_merge/addon.setup.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once PATH_THIRD."hop_social_merge/config.php";
+
+return array(
+	'author'      => 'Hop Studios',
+	'author_url'  => 'http://hopstudios.com',
+	'name'        => 'Hop Social Merge',
+	'description' => 'Hop Social Merge is fetching posts from social networks and combine them into one timeline.',
+	'docs_url' => 'http://www.hopstudios.com/software/hop_social_merge/docs',
+	'version'     => HOP_SOCIAL_MERGE_VERSION,
+	'namespace'   => 'HopStudios\HopSocialMerge',
+	'settings_exist' => TRUE
+);
---
+++ ee3x/system/user/addons/hop_social_merge/language/english/hop_social_merge_lang.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(
+
+	//Required for MODULES page
+
+	'hop_social_merge_module_name'			=> 'Hop Social Merge',
+	'hop_social_merge_module_description'	=> 'Display and merge into one timeline your posts from Twitter and Facebook',
+
+	'nav_settings'							=> 'Settings',
+
+	'settings_save'							=> 'Save Settings',
+	'settings_saved_success'				=> 'Settings have been saved successfully',
+	'settings_form_error_cache'				=> 'Cache must be an integer greater than 0',
+	//END
+);
