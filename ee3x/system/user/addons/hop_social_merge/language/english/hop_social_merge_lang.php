68d8450e23d2a600c7d85e53a2ef85611fb8f1b8
Louis DEKEISTER <louis@hopstudios.com>
2016-02-17 17:08:05 -0800

    Use EE3 settings api and validator

--- ee3x/system/user/addons/hop_social_merge/language/english/hop_social_merge_lang.php
+++ ee3x/system/user/addons/hop_social_merge/language/english/hop_social_merge_lang.php
@@ -13,6 +13,7 @@ $lang = array(
 
 	'label_cache_ttl'						=> 'Cache Life',
 	'label_sub_cache_ttl'					=> 'Time in minutes before refreshing data. Be careful, loading data from social networks can be long.',
+	'label_cache_ttl_unit'					=> 'Minutes',
 	'label_fcbk_app_token'					=> 'Facebook App Token',
 	'label_sub_fcbk_app_token'				=> 'The Facebook app token in order to access Facebook posts.',
 	'label_twitter_token'					=> 'Twitter Token',
@@ -24,7 +25,8 @@ $lang = array(
 	'label_twitter_cons_key_secret'			=> 'Twitter Consumer Key Secret',
 	'label_sub_twitter_cons_key_secret'		=> 'Twitter consumer key secret found on your developer app page',
 
-	'settings_save'							=> 'Save Settings',
+	'settings_save'							=> 'Save',
+	'settings_save_working'					=> 'Saving...',
 	'settings_saved_success'				=> 'Settings have been saved successfully',
 	'settings_form_error_cache'				=> 'Cache must be an integer greater than 0',
 	//END
--- ee3x/system/user/addons/hop_social_merge/mcp.hop_social_merge.php
+++ ee3x/system/user/addons/hop_social_merge/mcp.hop_social_merge.php
@@ -41,50 +41,122 @@ class hop_social_merge_mcp
     function settings()
     {
         $this->build_nav();
-        ee()->view->cp_page_title = lang('nav_settings');
-        $vars = array();
+		
+		$settings = Hop_social_merge_helper::get_settings();
+		
+		$vars = array(
+			'cp_page_title' => lang('nav_settings'),
+			'base_url' => ee('CP/URL', 'addons/settings/'.HOP_SOCIAL_MERGE_NAME.'/settings')->compile(),
+			'save_btn_text' => lang('settings_save'),
+			'save_btn_text_working' => lang('settings_save_working'),
+		);
+		
+		// Using EE3 API to create config form
+		$vars['sections'] = array(
+			array(
+				array(
+					'title' => 'label_cache_ttl',
+					'desc' => 'label_sub_cache_ttl',
+					'fields' => array(
+						'cache_ttl' => array('type' => 'short-text', 'required' => 'true', 'value' => $settings['cache_ttl'], 'label' => lang('label_cache_ttl_unit'))
+					)
+				),
+				array(
+					'title' => 'label_fcbk_app_token',
+					'desc' => 'label_sub_fcbk_app_token',
+					'fields' => array(
+						'facebook_app_token' => array('type' => 'text', 'value' => $settings['facebook_app_token'])
+					)
+				),
+				array(
+					'title' => 'label_twitter_token',
+					'desc' => 'label_sub_twitter_token',
+					'fields' => array(
+						'twitter_token' => array('type' => 'text', 'value' => $settings['twitter_token'])
+					)
+				),
+				array(
+					'title' => 'label_twitter_token_secret',
+					'desc' => 'label_sub_twitter_token_secret',
+					'fields' => array(
+						'twitter_token_secret' => array('type' => 'text', 'value' => $settings['twitter_token_secret'])
+					)
+				),
+				array(
+					'title' => 'label_twitter_cons_key',
+					'desc' => 'label_sub_twitter_cons_key',
+					'fields' => array(
+						'twitter_consumer_key' => array('type' => 'text', 'value' => $settings['twitter_consumer_key'])
+					)
+				),
+				array(
+					'title' => 'label_twitter_cons_key_secret',
+					'desc' => 'label_sub_twitter_cons_key_secret',
+					'fields' => array(
+						'twitter_consumer_secret' => array('type' => 'text', 'value' => $settings['twitter_consumer_secret'])
+					)
+				),
+				array(
+					'title' => '',
+					'fields' => array(
+						'action' => array('type' => 'hidden', 'value' => 'save_settings')
+					)
+				),
+			)
+		);
+		
         if (ee()->input->post('action') == "save_settings")
         {
             $settings = array();
             $form_is_valid = TRUE;
-            if (ee()->input->post('cache_ttl') != "" && is_numeric(ee()->input->post('cache_ttl')) && intval(ee()->input->post('cache_ttl')) > 0 )
-            {
-                $settings["cache_ttl"] = intval(ee()->input->post('cache_ttl'));
-            }
-            else
-            {
-                $settings["cache_ttl"] = ee()->input->post('cache_ttl');
-                $form_is_valid = FALSE;
-                $vars["form_error_cache"] = lang('settings_form_error_cache');
-            }
+			
+			// Validation
+			$validator = ee('Validation')->make();
+			
+			$validator->setRules(array(
+				'cache_ttl' => 'required|isNaturalNoZero',
+			));
+			$result = $validator->validate($_POST);
+			
+			if ($result->isValid())
+			{
+				// Get back all values, store them in array and save them
+				$fields = array();
+				foreach ($vars['sections'] as $settings)
+				{
+					foreach ($settings as $setting)
+					{
+						foreach ($setting['fields'] as $field_name => $field)
+						{
+							$fields[$field_name] = ee()->input->post($field_name);
+						}
+					}
+				}
+				// We don't want to save that field, it's not a setting
+				unset($fields['action']);
+				
+				Hop_social_merge_helper::save_settings($fields);
+				
+				ee('CP/Alert')->makeInline('shared-form')
+						->asSuccess()
+						->withTitle(lang('preferences_updated'))
+						->addToBody(lang('preferences_updated_desc'))
+						->defer();
 
-            $settings['facebook_app_token'] = ee()->input->post('facebook_app_token');
-            $settings['twitter_token'] = ee()->input->post('twitter_token');
-            $settings['twitter_token_secret'] = ee()->input->post('twitter_token_secret');
-            $settings['twitter_consumer_key'] = ee()->input->post('twitter_consumer_key');
-            $settings['twitter_consumer_secret'] = ee()->input->post('twitter_consumer_secret');
-
-            if ($form_is_valid)
-            {
-                Hop_social_merge_helper::save_settings($settings);
-                ee()->session->set_flashdata('message_success', lang('settings_saved_success'));
-                ee()->functions->redirect(ee('CP/URL')->make('addons/settings/'.HOP_SOCIAL_MERGE_NAME.'/settings'));
-            }
-            else
-            {
-                $vars["settings"] = $settings;
-            }
+				ee()->functions->redirect(ee('CP/URL')->make('addons/settings/'.HOP_SOCIAL_MERGE_NAME.'/settings'));
+			}
+			else
+			{
+				$vars['errors'] = $result;
+				ee('CP/Alert')->makeInline('shared-form')
+					->asIssue()
+					->withTitle(lang('settings_save_error'))
+					->addToBody(lang('settings_save_error_desc'))
+					->now();
+				$vars["settings"] = $settings;
+			}
 
         }
-
-        // No data received, means we'll load saved settings
-        if (!isset($form_is_valid))
-        {
-            $vars["settings"] = Hop_social_merge_helper::get_settings();
-        }
-
-        $vars['action_url'] = ee('CP/URL')->make('addons/settings/'.HOP_SOCIAL_MERGE_NAME.'/settings');
-        $vars['form_hidden'] = array('action' => 'save_settings');
 
         // return ee()->load->view('settings', $vars, TRUE);
 		return array(
