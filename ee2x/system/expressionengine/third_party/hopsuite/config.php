a7f693f590f97c804bd2d9726df63e4b7c97a897
Louis Dekeister <louis@hopstudios.com>
2017-09-08 11:44:50 -0700

    bump version to 1.1.2

--- ee2x/system/expressionengine/third_party/hopsuite/config.php
+++ ee2x/system/expressionengine/third_party/hopsuite/config.php
@@ -9,12 +9,12 @@
  * @author		Hop Studios, Inc.
  * @copyright	Copyright (c) 2015, Hop Studios, Inc.
  * @link		http://www.hopstudios.com/software
- * @version		1.1.1
+ * @version		1.1.2
  * @filesource	hopsuite/config.php
  */
 
 $config['name']='Hopsuite';
-$config['version']='1.1.1';
+$config['version']='1.1.2';
 $config['nsm_addon_updater']['versions_xml']='http://www.hopstudios.com/software/versions/hopsuite';
 
 // Version constant
